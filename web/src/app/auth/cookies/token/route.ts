7fa7d08207f6c9f97c166aa0aaa529f913f5ee28
apocsenpai <jonatassennas@ufrrj.br>
2023-07-08 00:01:30 -0300

    style:  run lint --fix

--- web/src/app/auth/cookies/token/route.ts
+++ web/src/app/auth/cookies/token/route.ts
@@ -1,18 +1,18 @@
 import { NextRequest, NextResponse } from 'next/server'
 
 export async function GET(request: NextRequest) {
-  const { searchParams } = new URL(request.url)
+	const { searchParams } = new URL(request.url)
 
-  const token = searchParams.get('token')
-  const redirectPath = searchParams.get('redirect')
+	const token = searchParams.get('token')
+	const redirectPath = searchParams.get('redirect')
 
-  const redirectUrl = new URL(redirectPath ? redirectPath : '/', request.url)
+	const redirectUrl = new URL(redirectPath || '/', request.url)
 
-  const cookieExpiresInSeconds = 24 * 60 * 60
+	const cookieExpiresInSeconds = 24 * 60 * 60
 
-  return NextResponse.redirect(redirectUrl, {
-    headers: {
-      'Set-Cookie': `token=${token}; Path=/; max-age=${cookieExpiresInSeconds};`,
-    },
-  })
+	return NextResponse.redirect(redirectUrl, {
+		headers: {
+			'Set-Cookie': `token=${token}; Path=/; max-age=${cookieExpiresInSeconds};`,
+		},
+	})
 }
--- web/src/components/Navbar/index.tsx
+++ web/src/components/Navbar/index.tsx
@@ -6,19 +6,19 @@ import { MdMenu } from '@react-icons/all-files/md/MdMenu'
 import Menu from './Menu'
 
 export default function NavBar() {
-  const [toggleMenu, setToggleMenu] = useState(false)
+	const [toggleMenu, setToggleMenu] = useState(false)
 
-  const handleToggleMenu = () => setToggleMenu(!toggleMenu)
+	const handleToggleMenu = () => setToggleMenu(!toggleMenu)
 
-  return (
-    <header className="flex h-28 w-full items-center justify-between px-20">
-      <button className="text-4xl text-alternative" onClick={handleToggleMenu}>
-        <MdMenu />
-      </button>
-      <Link href={'/'}>
-        <img src="images/miniLogo.svg" alt="Salve!" />
-      </Link>
-      <Menu handleToggleMenu={handleToggleMenu} active={toggleMenu} />
-    </header>
-  )
+	return (
+		<header className="flex h-28 w-full items-center justify-between px-20">
+			<button className="text-4xl text-alternative" onClick={handleToggleMenu}>
+				<MdMenu />
+			</button>
+			<Link href={'/'}>
+				<img src="images/miniLogo.svg" alt="Salve!" />
+			</Link>
+			<Menu handleToggleMenu={handleToggleMenu} active={toggleMenu} />
+		</header>
+	)
 }
